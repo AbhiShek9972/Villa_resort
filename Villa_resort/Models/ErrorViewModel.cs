633bda2b6e3f76a1bc6b2a660a6b14ed2aebf59c
Abhishek <abhishekrs034@gmail.com>
2025-08-11 21:34:46 +0530

    Add project files.

---
+++ Villa_resort/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Villa_resort.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
