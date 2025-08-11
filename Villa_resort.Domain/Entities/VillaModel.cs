8dcfd8c41bbd4e8d979df47c18cc192ce5f66704
Abhishek <abhishekrs034@gmail.com>
2025-08-11 22:00:19 +0530

    Model I added one folder entities and villamodel class

---
+++ Villa_resort.Domain/Entities/VillaModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Villa_resort.Domain.Entities
+{
+    public class VillaModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public Int32 Occupancy { get; set; }
+        public double Price { get; set; }
+        public Int32 Sqft { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; } = DateTime.Now;
+
+    }
+}
